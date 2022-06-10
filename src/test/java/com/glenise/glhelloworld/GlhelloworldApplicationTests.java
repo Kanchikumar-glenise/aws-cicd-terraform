5d617e7c851ecc1ffc09917c72353ebf7fb92b83
Kanchikumar-glenise <kanchikumar@glenise.in>
2022-06-10 15:03:23 +0530

    migrating r4

---
+++ src/test/java/com/glenise/glhelloworld/GlhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenise.glhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest.context;
+
+@SpringBootTest
+class GlhelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
